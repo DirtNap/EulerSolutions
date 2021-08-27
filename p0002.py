b24b26b30ac39f26503eec4e7bd5807abecfeab5
Michael Donnelly <MichaelVDonnelly@gmail.com>
2021-08-26 21:24:23 -0400

    Problem 2

---
+++ p0002.py
@@ -0,0 +1,16 @@
+import typing
+
+import util
+
+def fibonacci_sum(limit: int, filter: typing.Callable=lambda x: True):
+    result = 0
+    for current in util.fibonacci_generator(limit):
+        if filter(current):
+            result += current
+    return result
+
+def main():
+    print(f'The sum of even fibonacci numbers less than 4000000 is {fibonacci_sum(4000000, lambda x: x % 2 == 0)}')
+
+if __name__ == '__main__':
+    main()
---
+++ util.py
@@ -0,0 +1,27 @@
+import collections
+def prime_generator(upper_limit: int, start: int=2):
+    """Generate a list of prime numbers, starting with start, which are less than upper_limit."""
+    found = collections.defaultdict(list)
+    current = start
+    while current < upper_limit:
+        if current in found:
+            for factor in found[current]:
+                found[factor + current].append(factor)
+            del found[current]
+        else:
+            found[current * current].append(current)
+            yield current
+        current += 1
+
+def fibonacci_generator(upper_limit):
+    memoized = {1: 1, 2: 2}
+    def fib(n):
+        if n in memoized:
+            return memoized[n]
+        memoized[n] = fib(n - 1) + fib(n - 2)
+        return memoized[n]
+    for i in range(1, upper_limit):
+        if (current := fib(i)) < upper_limit:
+            yield current
+        else:
+            return
